f9a2c3e3072a7b6b2f37cf6bcd7dc3c71810222f
Maxim Shpak <max.dw.i.git@outlook.com>
2026-06-21 19:39:06 +0300

    Add extension signing to 'build' script

--- build-release.ps1
+++ build-release.ps1
@@ -1,13 +1,40 @@
-rimraf build
-rimraf web-ext-artifacts
+param (
+    [switch]$Sign
+)
+
+
+# Read '.env' file
+Get-Content .env | Where-Object { $_ -match '=' -and $_ -notmatch '^#' } | Foreach-Object {
+    $name, $value = $_.Split('=', 2)
+    Set-Item "env:$($name.Trim())" $value.Trim()
+}
+
+
+# Build the extension file
+rimraf $env:BUILD_DIR
+rimraf $env:ARTIFACTS_DIR
 
 Copy-Item -Force -Recurse ./static/icons ./src
 npm run lint
 npm run test
 webpack --config ./webpack.prod
-Copy-Item -Force -Recurse ./static/icons ./build
-web-ext build -s ./build/
+Copy-Item -Force -Recurse ./static/icons $env:BUILD_DIR
+web-ext build -s $env:BUILD_DIR/
 
-Compress-Archive -Path ./docs,./src,./package.json,./package-lock.json -DestinationPath ./web-ext-artifacts/source.zip
+Compress-Archive -Path ./docs,./src,./package.json,./package-lock.json -DestinationPath $env:ARTIFACTS_DIR/source.zip
 
 rimraf ./src/icons
+
+
+# Sign the extension file
+if ($Sign) {
+    $credentials = & "$env:KEEPASS_CLI_EXE" show -a username -a password $env:KEEPASS_DB $env:KEEPASS_ENTITY
+    if ($credentials.Count -lt 2) {
+        Write-Error "Could not extract tokens for extension signing"
+        exit 1
+    }
+    $issuer = $credentials[0].Trim()
+    $secret = $credentials[1].Trim()
+
+    web-ext sign --api-key=$issuer --api-secret=$secret --channel=unlisted --source-dir=$env:BUILD_DIR --artifacts-dir=$env:ARTIFACTS_DIR
+}
